c0bc0d15f22d595a56ad4c3116b8dcfdae7022db
Valerii Todua <todua.valeriy@gmail.com>
2025-07-13 18:48:35 +0400

    added preferences of units

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiohttp import ClientSession
 
@@ -7,12 +7,39 @@ BOT_TOKEN = ''
 
 WEATHER_API_KEY = ''
 
+users: dict[int, dict['str', bool]] = {}
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def process_start(message:Message):
-    await message.answer(f"🌤️ Welcome to WeatherWise Bot! 🌤️\nHi {message.from_user.first_name} I'm your personal weather assistant, ready to help you stay informed about the weather anywhere in the world.") # type: ignore
+    if message.from_user.id not in users: # type: ignore
+        users[message.from_user.id ] = { # type: ignore
+            'celsiusOrFahrenheit':True, #when True bot shows in celsius if False in Fahrenheit
+            'kilolometersOrMiles':True  #when True bot shows in kilometers if Fasle in Miles
+        }
+        print(users)
+
+    await message.answer(f"🌤️ Welcome to WeatherWise Bot! 🌤️\nHi {message.from_user.first_name} I'm your personal weather assistant, ready to help you stay informed about the weather anywhere in the world.\n Press /help to see how work with bot") # type: ignore
+
+@dp.message(Command(commands='change_temperature_units'))
+async def change_temperatue_units(message:Message):
+    users[message.from_user.id]['celsiusOrFahrenheit'] =  not  users[message.from_user.id]['celsiusOrFahrenheit'] # type: ignore
+
+    if users[message.from_user.id]['celsiusOrFahrenheit']: # type: ignore
+        await message.answer('now temperature units is celsius')
+    else:
+        await message.answer('now temperature units is fahrenheit')
+
+@dp.message(Command(commands='change_messure_units'))
+async def change_messure_units(message:Message):
+    users[message.from_user.id]['kilolometersOrMiles'] =  not  users[message.from_user.id]['kilolometersOrMiles'] # type: ignore
+
+    if users[message.from_user.id]['kilolometersOrMiles']: # type: ignore
+        await message.answer('now messure units are kilometers')
+    else:
+        await message.answer('now messure units are miles')
 
 @dp.message()
 async def process_current(message:Message):
@@ -23,7 +50,23 @@ async def process_current(message:Message):
             if response.status == 200:
                 data = await response.json()
                 #print(data)
-                await message.answer(f"🌤️**Current Weather in {data['location']['name']}, {data['location']['country']}**\n\n🌡️**{data['current']['temp_c']}°C** (feels like {data['current']['feelslike_c']}°C)\n\n {data['current']['condition']['text']}\n\n💧 Humidity: {data['current']['humidity']}%\n🌬️ Wind: {data['current']['wind_kph']} km/h {data['current']['wind_dir']}\n👁️ Visibility: {data['current']['vis_km']} km")
+                answer_sting:str =f'🌤️**Current Weather in {data['location']['name']}, {data['location']['country']}**\n\n'
+
+                if users[message.from_user.id]['celsiusOrFahrenheit']: # type: ignore
+                    answer_sting+=f'🌡️**{data['current']['temp_c']}°C** (feels like {data['current']['feelslike_c']}°C)\n\n '
+                else:
+                    answer_sting+=f'🌡️**{data['current']['temp_f']}°F * (feels like {data['current']['feelslike_f']}°F)\n\n'
+
+                answer_sting += f'{data['current']['condition']['text']}\n\n💧 Humidity: {data['current']['humidity']}%\n'
+
+                if users[message.from_user.id]['kilolometersOrMiles']: # type: ignore
+                    answer_sting += f'🌬️ Wind: {data['current']['wind_kph']} km/h {data['current']['wind_dir']}\n👁️ Visibility: {data['current']['vis_km']} km'
+                else:
+                    answer_sting += f'🌬️ Wind: {data['current']['wind_mph']} m/h {data['current']['wind_dir']}\n👁️ Visibility: {data['current']['vis_miles']} m'
+
+                await message.answer(answer_sting)
+            else:
+                await message.answer('something went wrong😢')
 
 
 dp.run_polling(bot)
